474a6de99d210da773c27a8e317873fa5a00cee2
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-22 22:13:07 -0500

    Converting Arrays

---
+++ main.js
@@ -0,0 +1,20 @@
+let messageOne = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let reversedMessageOne = messageOne.split('').reverse().join('');
+console.log(reversedMessageOne);
+
+let messageTwo = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let reversedMessageTwo = messageTwo.split('').reverse().join('');
+console.log(reversedMessageTwo);
+
+let messageThree = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let reversedMessageThree = messageThree.split('').reverse().join('');
+console.log(reversedMessageThree);
+
+
+let messageFour = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+let reversedMessageFour = messageFour.split('').reverse().join('');
+console.log(reversedMessageFour);
+
+let inspirationalQuote = "The happiness of your life depends on the quality of your thoughts.- Marcus Aurelius";
+let reversedInspirationalQuote = inspirationalQuote.split('').reverse().join('');
+console.log(reversedInspirationalQuote);
